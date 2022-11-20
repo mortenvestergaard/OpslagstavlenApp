0bb31ee70dfc4cbd40e42fe6518f6ab7c0748f3f
Morten Vestergaard <mbvestergaard@hotmail.com>
2022-11-20 21:46:53 +0100

    work

--- app/src/main/java/com/example/opslagstavlenapp/MainActivity.java
+++ app/src/main/java/com/example/opslagstavlenapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,7 @@ import android.widget.Toast;
 //OK Http3
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -26,18 +28,21 @@ import okhttp3.Response;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
     Button CameraButton;
     ImageView PictureView;
-    TextView txtString;
     Button GetImageButton;
+
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    public String apiUrl = "http://192.168.0.10:5158/api/";
+    public String takenPicture;
 
     OkHttpClient client = new OkHttpClient();
-    public String apiUrl = "http://192.168.0.12/api/";
+
 
     public static final int RequestPermissionCode = 1;
     @Override
@@ -48,34 +53,42 @@ public class MainActivity extends AppCompatActivity {
         PictureView = findViewById(R.id.PictureView);
         GetImageButton = findViewById(R.id.GetImagesButton);
         EnableRuntimePermission();
+
         CameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent, 7);
             }
-
-
         });
 
         GetImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
-                    run();
+                    GetRequest();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
-
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 7 && resultCode == RESULT_OK) {
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-            PictureView.setImageBitmap(bitmap);
+            Bitmap picture = (Bitmap) data.getExtras().get("data");
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            picture.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream.toByteArray();
+            takenPicture = Base64.encodeToString(byteArray, Base64.NO_WRAP);
+            PictureView.setImageBitmap(picture);
+            try {
+                PostRequest(apiUrl, takenPicture);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -94,8 +107,12 @@ public class MainActivity extends AppCompatActivity {
 
         RequestBody body = RequestBody.create(JSON, postBody);
 
+        //.Builder urlBuilder = HttpUrl.parse(apiUrl + "Image/PostImage").newBuilder();
+        //urlBuilder.addQueryParameter("imageString", postBody);
+        //String builtUrl = urlBuilder.build().toString();
+
         Request request = new Request.Builder()
-                .url(postUrl + "image/PostImage")
+                .url(postUrl + "Image/PostImage")
                 .post(body)
                 .build();
 
@@ -111,12 +128,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    void run() throws IOException {
+    void GetRequest() throws IOException {
 
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
-                .url(apiUrl + "image/GetImages")
+                .url(apiUrl + "Image/GetImages")
                 .build();
 
         client.newCall(request).enqueue(new Callback() {
@@ -133,7 +150,6 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        txtString.setText(myResponse);
                     }
                 });
 
